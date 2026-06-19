1f879f4b8a528d6cf04fdaa9648e8c2c4f49dd5c
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-06-19 12:51:44 +0300

    boolean checker

---
+++ javascript/freecodecamp/functions/booleanChecker.js
@@ -0,0 +1,16 @@
+const booWho = (arg) => {
+    return typeof arg === "boolean"
+}
+
+console.log(booWho('true'))
+// In this lab, you will build a function that checks if a value is classified as a boolean primitive.
+
+// Boolean primitives are true and false.
+
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// You should have a function called booWho that receives one argument.
+// If the argument received is a boolean primitive, the function should return true.
+// If the argument is any other value, the function should return false.
