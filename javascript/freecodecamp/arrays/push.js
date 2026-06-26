81592ffe5211b4419bc204520e50f606b036e33b
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-06-26 14:46:12 +0300

    push()

--- javascript/freecodecamp/arrays/push.js
+++ javascript/freecodecamp/arrays/push.js
@@ -1,4 +1,4 @@
 let fruits = ["Banana", "Lemon", "Oranges", "Mangos"]
 const newlength = fruits.push("Date", "kiwi")
 console.log(fruits)
-console.log(newlength) // the return value of pop() method is the new length of the array
+console.log(newlength) // the return value of push() method is the new length of the array
