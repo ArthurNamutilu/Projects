8f21a8a8cab612c5ca1a1b19ba884baac1cad9c1
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-08-26 13:12:21 +0300

    creating objects

---
+++ javascript/exercises/objects-Fundamentals.js
@@ -0,0 +1,16 @@
+// Creating Objects
+
+const user = {
+    name: "Fred",
+    age: 26,
+    isAdmin: false,
+    skills: ["JavaScript", "React","Next JS"],
+    address: {
+        city: "Riyadh",
+        country: "Saudi Arabia"
+    },
+    greet: function(){
+        return "Hello";
+    }
+};
+
