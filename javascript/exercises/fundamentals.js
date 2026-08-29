8b586b15e07e55174edb20699e832ba01774cda0
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-08-29 06:32:07 +0300

    js object fundamentals

---
+++ javascript/exercises/fundamentals.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "Fred Dela Mayne",
+    age: 27,
+    country: "KSA",
+    occupation: "Backend Developer"
+}
+
+console.log(`Meet ${person.name} a ${person.occupation} in ${person.country}`);
+
+const car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2024
+};
+
+car.year = 2025;
+car.color = "black"
+
+console.log(car);
