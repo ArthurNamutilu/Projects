fd94889c5dfa1d5418f95fbf3198b049764b3782
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-08-29 12:58:46 +0300

    maping in array

---
+++ javascript/exercises/object+array.js
@@ -0,0 +1,9 @@
+const users = [
+    { name: "Arthur", age: 25 },
+    { name: "John", age: 30 },
+    { name: "Mary", age: 22 }
+];
+
+const names = users.map(user => user.name )
+
+console.log(names);
