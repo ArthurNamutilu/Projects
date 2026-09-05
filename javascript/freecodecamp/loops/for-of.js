dc4623b6757ade02f703043a5c0a8b95fabad87c
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-09-05 14:30:53 +0300

    for of loop

---
+++ javascript/freecodecamp/loops/for-of.js
@@ -0,0 +1,8 @@
+//for...of loop is used when you need to loop over values from an iterable
+
+const fruits = ['apple', 'banana', 'cherry'];
+
+for (let fruit of fruits) {
+  console.log(fruit.toUpperCase());
+}
+
