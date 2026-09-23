0facff6a8f22741122a7429946c74d9df6500cfc
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-09-23 08:38:18 +0300

    body styling

--- React/scrimba-projects/src/components/Main.jsx
+++ React/scrimba-projects/src/components/Main.jsx
@@ -1,6 +1,17 @@
 export default function Main(){
     return(
-            <>Hi</>
+            <>
+                <main>
+                    <h1>Fun facts about React</h1>
+                    <ul className="facts-list">
+                        <li>Was first released in 2013</li>
+                        <li>Was originally created by Jordan Walke</li>
+                        <li>Has well over 100K stars on GitHub</li>
+                        <li>Is maintained by Meta</li>
+                        <li>Powers thousands of enterprise apps including mobile</li>
+                    </ul>
+                </main>
+            </>
         )
     
 }
